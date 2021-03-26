233fa791e2bae855c622d9fa6a69dbfd930e6cbb
Andrey Skobelev <dre0druofficial@gmail.com>
2021-03-27 00:18:53 +0300

    feat!: removed implementations that provide too much abstraction

--- Packages/AddressablesServices/Runtime/Loaders/AddressablesObjectLoader.cs
+++ Packages/AddressablesServices/Runtime/Loaders/AddressablesObjectLoader.cs
@@ -3,9 +3,9 @@ using UnityEngine.AddressableAssets;
 
 namespace AddressablesServices.Loaders
 {
-    public abstract class
-        BaseAddressablesObjectLoader<TAssetReference, TResult> : BaseAddressablesLoader<TAssetReference, TResult,
-            TResult> where TAssetReference : AssetReference where TResult : Object
+    public class AddressablesObjectLoader<TAssetReference, TResult> :
+        AddressablesLoaderBase<TAssetReference, TResult, TResult>
+        where TAssetReference : AssetReference where TResult : Object
     {
         protected override bool TryExtractResult(TResult loadedAsset, out TResult result)
         {

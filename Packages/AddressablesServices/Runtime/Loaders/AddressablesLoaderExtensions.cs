e0eb22f05a5f6ebb865ebebef73eec6e4df471d1
Andrey Skobelev <dre0druofficial@gmail.com>
2021-04-09 20:49:27 +0300

    feat: extracted `params` overload for `IAddressablesLoader` to extension methods, added extension methods for assets loading

--- Packages/AddressablesServices/Runtime/Loaders/AddressablesLoaderExtensions.cs
+++ Packages/AddressablesServices/Runtime/Loaders/AddressablesLoaderExtensions.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -26,5 +29,84 @@ namespace AddressablesServices.Loaders
         {
             return addressablesLoader.GetAsset(assetReference).GetComponent<T>();
         }
+
+        public static UniTask PreloadAssetsAsync<TAsset>(this IAddressablesLoader<TAsset> addressablesLoader,
+            params AssetReferenceT<TAsset>[] assetReferences)
+            where TAsset : Object
+        {
+            return addressablesLoader.PreloadAssetsAsync(assetReferences);
+        }
+
+        public static void UnloadAssets<TAsset>(this IAddressablesLoader<TAsset> addressablesLoader,
+            params AssetReferenceT<TAsset>[] assetReferences)
+            where TAsset : Object
+        {
+            addressablesLoader.UnloadAssets(assetReferences);
+        }
+
+        public static async UniTask<TAsset> LoadAssetAsync<TAsset>(
+            this IAddressablesLoader<TAsset> addressablesLoader,
+            AssetReferenceT<TAsset> assetReference)
+            where TAsset : Object
+        {
+            if (addressablesLoader.IsAssetPreloaded(assetReference))
+            {
+                return addressablesLoader.GetAsset(assetReference);
+            }
+
+            await addressablesLoader.PreloadAssetAsync(assetReference);
+
+            return addressablesLoader.GetAsset(assetReference);
+        }
+
+        public static UniTask<IEnumerable<TAsset>> LoadAssetsAsync<TAsset>(
+            this IAddressablesLoader<TAsset> addressablesLoader,
+            IEnumerable<AssetReferenceT<TAsset>> assetReferences)
+            where TAsset : Object
+        {
+            return addressablesLoader.LoadAssetsAsync(assetReferences.ToArray());
+        }
+
+        public static async UniTask<IEnumerable<TAsset>> LoadAssetsAsync<TAsset>(
+            this IAddressablesLoader<TAsset> addressablesLoader,
+            params AssetReferenceT<TAsset>[] assetReferences)
+            where TAsset : Object
+        {
+            var assetsToLoad = assetReferences
+                .Where(assetReference => addressablesLoader.IsAssetPreloaded(assetReference) == false);
+
+            await addressablesLoader.PreloadAssetsAsync(assetsToLoad);
+
+            return addressablesLoader.GetAssets(assetReferences);
+        }
+
+        public static IEnumerable<TAsset> GetAssets<TAsset>(this IAddressablesLoader<TAsset> addressablesLoader,
+            IEnumerable<AssetReferenceT<TAsset>> assetReferences)
+            where TAsset : Object
+        {
+            return assetReferences.Select(addressablesLoader.GetAsset);
+        }
+
+        public static bool TryGetAssets<TAsset>(this IAddressablesLoader<TAsset> addressablesLoader,
+            IEnumerable<AssetReferenceT<TAsset>> assetReferences, out IEnumerable<TAsset> assets)
+            where TAsset : Object
+        {
+            return addressablesLoader.TryGetAssets(out assets, assetReferences.ToArray());
+        }
+        
+        public static bool TryGetAssets<TAsset>(this IAddressablesLoader<TAsset> addressablesLoader,
+            out IEnumerable<TAsset> assets, params AssetReferenceT<TAsset>[] assetReferences)
+            where TAsset : Object
+        {
+            if (assetReferences.All(addressablesLoader.IsAssetPreloaded) == false)
+            {
+                assets = Enumerable.Empty<TAsset>();
+                return false;
+            }
+
+            assets = addressablesLoader.GetAssets(assetReferences);
+            return true;
+        }
+        
     }
 }

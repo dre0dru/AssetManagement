340e912db26f670ce403a522c575757e529add5d
Andrey Skobelev <dre0druofficial@gmail.com>
2021-05-21 18:53:54 +0300

    fix: update menu item name

--- Editor/AddressablesFullLogs.cs
+++ Editor/AddressablesFullLogs.cs
@@ -8,13 +8,13 @@ namespace AddressablesServices.Editor
     {
         private const string AddressablesFullLogsDefine = "ADDRESSABLES_LOG_ALL";
 
-        [MenuItem("Window/Asset Management/Addressables/Logging/Enable addressables full logs")]
+        [MenuItem("Window/Asset Management/Addressables/Logs/Enable addressables full logs")]
         public static void EnableAddressablesFullLogs()
         {
             ScriptingDefineUtility.Add(AddressablesFullLogsDefine);
         }
 
-        [MenuItem("Window/Asset Management/Addressables/Logging/Disable addressables full logs")]
+        [MenuItem("Window/Asset Management/Addressables/Logs/Disable addressables full logs")]
         public static void DisableAddressablesFullLogs()
         {
             ScriptingDefineUtility.Remove(AddressablesFullLogsDefine);

e8c14f9f97c2c02dc425f74a3005a3122808807f
Andrey Skobelev <dre0druofficial@gmail.com>
2021-05-21 18:43:02 +0300

    feat: moved to scripting define utility to optional dependencies

--- Editor/AddressablesFullLogs.cs
+++ Editor/AddressablesFullLogs.cs
@@ -1,4 +1,5 @@
-﻿using TNRD.Utilities;
+﻿#if SCRIPTING_DEFINE_UTILITY
+using TNRD.Utilities;
 using UnityEditor;
 
 namespace AddressablesServices.Editor
@@ -6,13 +7,13 @@ namespace AddressablesServices.Editor
     public static class AddressablesFullLogs
     {
         private const string AddressablesFullLogsDefine = "ADDRESSABLES_LOG_ALL";
-        
+
         [MenuItem("Window/Asset Management/Addressables/Logging/Enable addressables full logs")]
         public static void EnableAddressablesFullLogs()
         {
             ScriptingDefineUtility.Add(AddressablesFullLogsDefine);
         }
-        
+
         [MenuItem("Window/Asset Management/Addressables/Logging/Disable addressables full logs")]
         public static void DisableAddressablesFullLogs()
         {
@@ -20,3 +21,4 @@ namespace AddressablesServices.Editor
         }
     }
 }
+#endif
